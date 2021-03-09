af84825d298110390bec56e16cbce82307acf7f2
Ethan <martin.ethan08@gmail.com>
2021-03-08 18:24:09 -0600

    added the ability to process and respond to mentions with recommendations

--- SongBot.py
+++ SongBot.py
@@ -6,51 +6,98 @@ import spotipy.util as util
 import random
 import tweepy
 
-# Set user ID retreived from Spotify account.
-username = os.environ.get('SPOTIPY_USERID')
+# Sets up an Object that can interact with
+# the Spotify API.
+def spotifyAPISetup():
+    username = os.environ.get('SPOTIPY_USERID')
+    try:
+        token = util.prompt_for_user_token(username)
+    except:
+        os.remove(f".cache-{username}")
+        token = util.prompt_for_user_token(username)
+    spotifyClient = spotipy.Spotify(auth=token)
+    user = spotifyClient.current_user()
+    return spotifyClient
 
-try:
-    token = util.prompt_for_user_token(username)
-except:
-    os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username)
+# Sets up an Object that can interact with the
+# Twitter API through a specific client account.
+def twitterAPISetup():
+    consumer_key = os.environ.get('TWITTER_API_KEY')
+    consumer_secret = os.environ.get('TWITTER_API_KEY_SECRET')
+    key = os.environ.get('ACCESS_TOKEN')
+    secret = os.environ.get('ACCESS_TOKEN_SECRET')
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(key, secret)
+    twitterClient = tweepy.API(auth)
+    return twitterClient
 
-consumer_key = os.environ.get('TWITTER_API_KEY')
-consumer_secret = os.environ.get('TWITTER_API_KEY_SECRET')
-key = os.environ.get('ACCESS_TOKEN')
-secret = os.environ.get('ACCESS_TOKEN_SECRET')
+# Returns a list of any new mentions that have not been
+# replied to through the client Twitter account.
+def getMentions(twitterClient):
+    allMentions = twitterClient.mentions_timeline()
+    currMentions = []
+    if (len(allMentions) > 0):
+        count = 0
+        while (count < len(allMentions)) and (not allMentions[count].favorited):
+            currMentions.insert(0, allMentions[count])
+            count += 1
+    return currMentions
 
-# Create Twitter authorization instance
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(key, secret)
-    
-api = tweepy.API(auth)
-api.update_status('tweepy + oauth!')
+# Creates and returns a list of the users
+# that have mentioned the client Twitter account.
+# Parameter mentions represents all the mentions
+# that have not been replied to.
+def getUsers(mentions):
+    users = []
+    for i in range(len(mentions)):
+        nextUser = mentions[i].user.screen_name
+        users.insert(0, nextUser)
+    return users
 
+# Creates and returns a list of all the recommendation
+# requests that other Twitter users have made via mentions.
+# Parameter mentions represents all the mentions that have
+# not been replied to.
+def getTweets(twitterClient, mentions):
+    clientMention = "@" + twitterClient.me().screen_name
+    tweets = []
+    for i in range(len(mentions)):
+        twitterClient.create_favorite(mentions[i].id)
+        nextTweet = mentions[i].text.replace(clientMention, '')
+        tweets.insert(0, nextTweet)
+    return tweets
 
-# Create Spotipy object based off of user ID
-# stored in variable username.
-spotifyObject = spotipy.Spotify(auth=token)
-
-user = spotifyObject.current_user()
-while(True):
-    print()
-    artistQuery = input("Enter an artist's name for 5 song recommendations: ") # Prompt user input
-    artistID = spotifyObject.search(artistQuery,1,0,"artist") ['artists']['items'][0]['id']
+# Takes what the user requested and returns a string
+# of recommendations if there is valid input.
+# In the case of invalid inputs, the program will
+# catch the error and return an error message.
+def generateRecommendations(spotifyClient, user, tweet):
+    NUM_RECOMMENDATIONS = 4
+    result = "@" + user + ",\n"
+    try:
+        artistID = spotifyClient.search(tweet,1,0,"artist")['artists']['items'][0]['id']
+    except:
+        result += "Invalid input: please enter in the name of an artist!"
+        return result
     list = []
     list.append(artistID)
-    print()
-    trackList = spotifyObject.recommendations(list, None, None, 5, 'US') # Store 5 song recommendations
-    print("Here are five recommended songs (in no particular order):\n")
-    count = 0
-    while (count < 5):
-        trackName = trackList['tracks'][count]['name'] # Get track name
-        artistName = trackList['tracks'][count]['artists'][0]['name'] # Get artist name
-        print(count+1, ". " + trackName + " by " + artistName + "\n")
-        count = count + 1
-    userContinue = input("Would you like to search for another recommendation? (enter 'y' for yes. Any other key will end this program) ")
-    if (userContinue.lower() != 'y'):
-        break
+    trackList = spotifyClient.recommendations(list, None, None, NUM_RECOMMENDATIONS, 'US') # Generate 4 song recommendations
+    result += "Here are four recommended tracks for: \"" + tweet + "\"\n(in no particular order)\n"
+    for i in range(NUM_RECOMMENDATIONS):
+        trackName = trackList['tracks'][i]['name']
+        artistName = trackList['tracks'][i]['artists'][0]['name']
+        result += "\n" + str(i + 1) + ". " + trackName + " by " + artistName
+    return result
 
+# Main driver of the program
+def main():
+    spotifyClient = spotifyAPISetup()
+    twitterClient = twitterAPISetup()
+    mentions = getMentions(twitterClient)
+    users = getUsers(mentions)
+    tweets = getTweets(twitterClient, mentions)
+    for i in range(len(mentions)):
+        recommendation = generateRecommendations(spotifyClient, users[i], tweets[i])
+        twitterClient.update_status(recommendation)
 
-# print(json.dumps(VARIABLE, sort_keys=True, indent=4))
+main()
